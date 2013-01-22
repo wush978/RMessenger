1ba880ffb71ec50f1a4267ea34bf0a177f2c7db5
Wush Wu <wush978@gmail.com>
2013-01-22 11:41:43 +0800

    add facebookMessage

---
+++ R/facebookMessage.function.R
@@ -0,0 +1,15 @@
+#'Send Facebook Message
+#'
+#'@param username facebook username
+#'@param password password for login
+#'@param to a string of number of fbid
+#'@param message message to send
+#'@param log_level level of displayed message. One of XMPP_LEVEL_DEBUG, XMPP_LEVEL_INFO, XMPP_LEVEL_WARN, and XMPP_LEVEL_ERROR
+#'@examples
+#'facebookMessage("wush", "shouldnotbehere", "12345678", "R XMPPNotifier testing message")
+#'@export
+facebookMessage <- function(username, password, fbid, message, log_level = XMPP_LEVEL_ERROR) {
+	username <- paste(username, "@chat.facebook.com", sep="")
+	fbid <- paste("-", fbid, "@chat.facebook.com", sep="")
+	sendXMPPMessage(username, password, fbid, message, log_level)
+}
