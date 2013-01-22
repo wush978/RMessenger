788e85c286924b9421d61f5e4a1cfb3ef0750785
Wush Wu <wush978@gmail.com>
2013-01-22 12:03:50 +0800

    add timeout

--- R/facebookMessage.function.R
+++ R/facebookMessage.function.R
@@ -5,11 +5,12 @@
 #'@param to a string of number of fbid
 #'@param message message to send
 #'@param log_level level of displayed message. One of XMPP_LEVEL_DEBUG, XMPP_LEVEL_INFO, XMPP_LEVEL_WARN, and XMPP_LEVEL_ERROR
+#'@param timeout time to wait for sending message. The longest time for hanging is 2*timeout
 #'@examples
 #'facebookMessage("wush", "shouldnotbehere", "12345678", "R XMPPNotifier testing message")
 #'@export
-facebookMessage <- function(username, password, fbid, message, log_level = XMPP_LEVEL_ERROR) {
+facebookMessage <- function(username, password, fbid, message, log_level = XMPP_LEVEL_ERROR, timeout=15) {
 	username <- paste(username, "@chat.facebook.com", sep="")
 	fbid <- paste("-", fbid, "@chat.facebook.com", sep="")
-	sendXMPPMessage(username, password, fbid, message, log_level)
+	sendXMPPMessage(username, password, fbid, message, log_level, timeout)
 }
--- R/sendXMPPMessage.function.R
+++ R/sendXMPPMessage.function.R
@@ -5,11 +5,12 @@
 #'@param to "-<fbid>@@chat.facebook.com"
 #'@param message message to send
 #'@param log_level level of displayed message. One of XMPP_LEVEL_DEBUG, XMPP_LEVEL_INFO, XMPP_LEVEL_WARN, and XMPP_LEVEL_ERROR
+#'@param timeout time to wait for sending message. The longest time for hanging is 2*timeout
 #'@examples
 #'sendXMPPMessage("xxx@@chat.facebook.com", "xxx", "-12345xxx1324@@chat.facebook.com", "test message via R")
 #'@export
 #'
-sendXMPPMessage <- function(jid, password, to, message, log_level = XMPP_LEVEL_ERROR) {
+sendXMPPMessage <- function(jid, password, to, message, log_level = XMPP_LEVEL_ERROR, timeout = 15) {
 	agent <- new("Rcpp_RAgent", jid, password, log_level, 
 							 function() { 
 							 		agent$send(to, message)
@@ -23,6 +24,6 @@ sendXMPPMessage <- function(jid, password, to, message, log_level = XMPP_LEVEL_E
 	agent$addHandler( function() {
 		agent$stop()
 	}, "", "iq", "set")
-	agent$run()
+	agent$run(as.integer(timeout))
 	agent$Disconnect()
 }
