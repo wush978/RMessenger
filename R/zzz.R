f6ff3e4d27a2b2327249ae636c1a02e0c6051732
Wush Wu <wush978@gmail.com>
2013-01-29 20:40:57 +0800

    update documentation

--- R/zzz.R
+++ R/zzz.R
@@ -8,17 +8,11 @@
 #'@useDynLib RMessenger
 XMPP_LEVEL_DEBUG <- 0L
 
-#'Level for displaying message
-#'
 #'@export
 XMPP_LEVEL_INFO <- 1L
 
-#'Level for displaying message
-#'
 #'@export
 XMPP_LEVEL_WARN <- 2L
 
-#'Level for displaying message
-#'
 #'@export
 XMPP_LEVEL_ERROR <- 3L
