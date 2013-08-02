19e33ae61bb24c14e657bc816db8ecd5d2e94218
Wush Wu <wush978@gmail.com>
2013-08-02 13:39:00 +0800

    * checking Rd line widths ... NOTE Rd file ‘sendXMPPMessage.Rd’:   \examples lines wider than 100 characters:      sendXMPPMessage("xxx@chat.facebook.com", "xxx", "-12345xxx1324@chat.facebook.com", "test message via R")
    These lines will be truncated in the PDF manual.

--- R/sendXMPPMessage.function.R
+++ R/sendXMPPMessage.function.R
@@ -15,14 +15,17 @@
 #'
 #'@param jid single length character such as "xxxxxx@@chat.facebook.com" or "xxxxx@@gmail.com"
 #'@param password single length character which is the password for \code{jid}
-#'@param to single length character which is the jid of receiver such as "-<fbid>@@chat.facebook.com" or "xxxx@@gmail.com"
+#'@param to single length character which is the jid of receiver such as 
+#'"-<fbid>@@chat.facebook.com" or "xxxx@@gmail.com"
 #'@param message multiple length character, the messages to \code{to}.
 #'@param log_level level of displayed message to R console. One of \code{\link{XMPP_LEVEL_DEBUG}}, 
 #'  \code{\link{XMPP_LEVEL_INFO}}, \code{\link{XMPP_LEVEL_WARN}}, and \code{\link{XMPP_LEVEL_ERROR}}
-#'@param timeout time(seconds) to wait for sending message. The longest time for blocking is \code{2*timeout}
+#'@param timeout time(seconds) to wait for sending message. 
+#'The longest time for blocking is \code{2*timeout}
 #'@examples
 #'\dontrun{
-#'sendXMPPMessage("xxx@@chat.facebook.com", "xxx", "-12345xxx1324@@chat.facebook.com", "test message via R")
+#'sendXMPPMessage("xxx@@chat.facebook.com", "xxx", "-12345xxx1324@@chat.facebook.com", 
+#'  "test message via R")
 #'}
 #'@export
 #'@seealso \code{\link{facebookMessage}} for sending message via facebook
